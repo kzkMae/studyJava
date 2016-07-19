6b737e076a94022d7304ae9037c57c43623670a2
kzkMae <16jkm23@ms.dendai.ac.jp>
2016-07-19 16:29:21 +0900

    練習問題2

---
+++ src/practice2.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Max on 2016/07/19.
+ */
+public class practice2 {
+    public static void main(String[] args) {
+        int ii;
+        float ff;
+        double dd = 1234.56789012;
+
+        ii = (int)dd;
+        ff = (float) dd;
+
+        System.out.println("ii="+ii);
+        System.out.println("ff="+ff);
+        System.out.println("dd="+dd);
+    }
+}
