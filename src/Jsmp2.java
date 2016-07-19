6245729910f7e16ff192c7d959f6ceb665c630f0
kzkMae <16jkm23@ms.dendai.ac.jp>
2016-07-19 11:03:48 +0900

    System.out.printlnコマンド

---
+++ src/Jsmp2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Max on 2016/07/19.
+ */
+public class Jsmp2 {
+    public static void main(String[] args) {
+        System.out.print("AAAAAA");
+        System.out.print("BBBBB\nCCCCCCCC\n");
+        System.out.println("DDDDDDDDDDD");
+        System.out.println("EEEEEEEEEE\nFFFFFFFFFFFF\n\nGGGGGGGGGGGGGGG");
+    }
+}
