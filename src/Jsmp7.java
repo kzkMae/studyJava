ca255cfa2cf9f34ad4dcf7bae6247ae977e77aed
kzkMae <16jkm23@ms.dendai.ac.jp>
2016-07-19 14:27:27 +0900

    If文

---
+++ src/Jsmp7.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Max on 2016/07/19.
+ */
+public class Jsmp7 {
+    public static void main(String[] args) {
+        int i;
+
+        for(i=1; i<=5; i++){
+            System.out.print("i="+i);
+            if (i<3)
+                System.out.println(" ３未満です");
+            else
+                System.out.println(" ３以上です");
+        }
+    }
+}
