53c41d1a549cd77d7a71b896a3eb4aa66b455fb5
kzkMae <16jkm23@ms.dendai.ac.jp>
2016-07-19 14:18:29 +0900

    For文

---
+++ src/Jsmp6.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Max on 2016/07/19.
+ */
+public class Jsmp6 {
+    public static void main(String[] args) {
+        int i,sum;
+
+        for(i=1; i<=5; i++)
+            System.out.println("i="+i);
+
+        sum = 0;
+        for(i=1; i<=5; i++){
+            sum = sum+i;
+            System.out.println("sum="+sum);
+        }
+    }
+}
