06219688b3ff40b7ac942d35b8bf876be073b68a
kzkMae <16jkm23@ms.dendai.ac.jp>
2016-07-19 11:15:07 +0900

    整数値計算

---
+++ src/Jsmp5.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Max on 2016/07/19.
+ */
+public class Jsmp5 {
+    public static void main(String[] args) {
+        int a,b,sum;
+
+        a = 100;
+        b = 200;
+        sum = a + b;
+        System.out.print("a = ");
+        System.out.println(a);
+        System.out.println("b = " + b);
+        System.out.println("a+b = " + sum);
+        System.out.println("a+b = " + (a+b));
+    }
+}
