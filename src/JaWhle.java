7eb41096ce5cdba3d83404e4f75a23c33b4dffab
kzkMae <16jkm23@ms.dendai.ac.jp>
2016-07-22 15:49:16 +0900

    While文

---
+++ src/JaWhle.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Max on 2016/07/22.
+ */
+public class JaWhle {
+    public static void main(String[] args) {
+        int i, sum;
+        int dt[] = {10, 20, 30, -1, 50};
+
+        System.out.println("------------(1) １～５の和を計算");
+        i = 1;
+        sum = 0;
+        while (i <=5){
+            sum = sum +i;
+            i++;
+        }
+        System.out.println("sum=" + sum);
+
+        System.out.println("----------(2) -1にであうまでの配列値を表示");
+        i=0;
+        while (dt[i] != -1){
+            System.out.println("i=" + i+ " dt[i]=" + dt[i]);
+            i++;
+        }
+    }
+}
